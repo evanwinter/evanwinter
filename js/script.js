7d373c22cee86021948ca97a026a68287d42a035
Evan Winter <ewinter1996@gmail.com>
2017-10-07 22:00:06 -0500

    .

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-$(".anchor").click(function(e){
+$(".js-anchor").click(function(e){
 	var scrollTarget = this.id;
 	var scrollTarget = "."+scrollTarget+"-section";
 	$("html, body").animate({ scrollTop: $(scrollTarget).offset().top-59 }, "slow");
