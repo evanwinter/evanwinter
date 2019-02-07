195179e05df63c799447e57367384c92845b9ef5
Evan Winter <ewinter@sccadv.com>
2019-02-06 23:20:11 -0600

    .

--- src/js/index.js
+++ src/js/index.js
@@ -21,65 +21,73 @@ const ice = "#F1FAEE"
 const lightTan = "floralwhite"
 const red = "#B80C09"
 const pink = "#FF8484"
+const orange = "orange"
 const green = "#239a3b"
 const papayawhip = "papayawhip"
 
 const THEMES = [
-	{ 
-		background: dark, 
-		color: light, 
-		linkColor: light 
+	{
+		background: dark,
+		color: light,
+		linkColor: light
 	},
-	{ 
-		background: light, 
-		color: dark, 
-		linkColor: dark 
+	{
+		background: light,
+		color: dark,
+		linkColor: dark
 	},
-	{ 
-		background: darkblue, 
-		color: light, 
-		linkColor: light 
+	{
+		background: darkblue,
+		color: light,
+		linkColor: light
 	},
-	{ 
-		background: green, 
-		color: light, 
-		linkColor: light 
+	{
+		background: green,
+		color: light,
+		linkColor: light
 	},
-	{ 
-		background: ice, 
-		color: dark, 
-		linkColor: dark 
+	{
+		background: ice,
+		color: dark,
+		linkColor: dark
 	},
-	{ 
-		background: pink, 
-		color: light, 
-		linkColor: light 
+	{
+		background: pink,
+		color: light,
+		linkColor: light
 	},
-	{ 
-		background: red, 
-		color: light, 
-		linkColor: light 
+	{
+		background: red,
+		color: light,
+		linkColor: light
 	},
-	{ 
-		background: tan, 
-		color: dark, 
-		linkColor: dark 
+	{
+		background: tan,
+		color: dark,
+		linkColor: dark
 	},
-	{ 
-		background: yellow, 
-		color: dark, 
-		linkColor: dark 
+	{
+		background: yellow,
+		color: dark,
+		linkColor: dark
 	},
-	{ 
-		background: papayawhip, 
-		color: dark, 
-		linkColor: dark 
+	{
+		background: papayawhip,
+		color: dark,
+		linkColor: dark
 	},
+	{
+		background: orange,
+		color: dark,
+		linkColor: dark
+	}
 ]
 
 class ThemeManager {
 	constructor() {
 		this.trigger = document.querySelector("#secret-button")
+		this.setTheme = this.setTheme.bind(this)
+		this.handleUpdate = this.handleUpdate.bind(this)
 	}
 
 	listen() {
@@ -92,21 +100,42 @@ class ThemeManager {
 
 	handleUpdate() {
 		// get a random themes
-		const randInt = randomIntFromInterval(0, THEMES.length-1)
+		const randInt = randomIntFromInterval(0, THEMES.length - 1)
 		const nextTheme = THEMES[randInt]
-		const { background, color, linkColor } = nextTheme
+
+		// set next theme
+		this.setTheme(nextTheme)
+	}
+
+	setTheme(theme) {
+		const { background, color, linkColor } = theme
 
 		// get target elements
 		const body = _("body")
+		const paragraphs = $("p")
 		const links = $("a")
 
 		// apply styles
 		body.style.background = background
 		body.style.color = color
-		links.forEach(link => link.style.color = linkColor)
-
+		links.forEach(link => (link.style.color = linkColor))
+		paragraphs.forEach(paragraph => (paragraph.style.color = color))
 	}
 
+	setInitialTheme() {
+		// set initial theme based on time of day (sort of)
+		const now = new Date()
+		const hours = now.getHours()
+		const numThemes = THEMES.length
+		// if hours is greater than number of themes, keep wrapping around the list
+		let n = hours
+		while (n >= numThemes) {
+			n = n - numThemes
+		}
+
+		// this.setTheme(THEMES[n])
+		this.setTheme(THEMES[1])
+	}
 }
 
 const randomIntFromInterval = (min, max) => {
@@ -117,23 +146,30 @@ document.addEventListener("DOMContentLoaded", () => {
 	main()
 })
 
-function main() {
-
-	// const hello = document.querySelector("#hello")
-	// setTimeout(() => {
-	// 	hello.classList.add("animate-in")
-	// }, 0)
-
-	// setTimeout(() => {
-	// 	hello.classList.remove("animate-in")
-	// }, 2000)
-
+function showSplash(duration) {
+	const hello = document.querySelector("#hello")
 	setTimeout(() => {
-		Array.from(document.querySelectorAll(".animate")).forEach(el => el.classList.add("animate-in"))
-	}, 100)
+		hello.classList.add("animate-in")
+	}, 0)
+	setTimeout(() => {
+		hello.classList.remove("animate-in")
+	}, duration / 2)
+}
 
+function main() {
 	// listen for theme changes
 	const themeManager = new ThemeManager()
+	// themeManager.setInitialTheme()
 	themeManager.listen()
 
+	const showSplashPage = false
+	const splashDuration = 3000
+	if (showSplashPage)
+		showSplash(splashDuration)
+	
+	setTimeout(() => {
+		Array.from(document.querySelectorAll(".animate")).forEach(el =>
+			el.classList.add("animate-in")
+		)
+	}, showSplashPage ? splashDuration : 1000)
 }
