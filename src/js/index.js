cc69312ff24586b118b4ad46199c5036cfd12bb2
Evan Winter <ewinter1996@gmail.com>
2019-08-28 09:30:57 -0500

    batch 2

--- src/js/index.js
+++ src/js/index.js
@@ -4,4 +4,28 @@ import "../scss/index.scss"
 // Detect characteristics of current device
 import device from "current-device"
 
-console.log("Running")
+// Import typography library
+import Typography from 'typography'
+
+const typography = new Typography({
+  baseFontSize: '16px',
+  baseLineHeight: 1.45,
+  headerFontFamily: ['Space Grotesk', '-apple-system', 'system-ui', 'sans-serif'],
+  bodyFontFamily: ['Space Grotesk', '-apple-system', 'system-ui', 'sans-serif'],
+})
+
+const setInvertedText = () => {
+	document.querySelectorAll("")
+}
+
+typography.injectStyles()
+
+document.addEventListener('DOMContentLoaded', (e) => {
+	const cube = document.querySelector(".cube")
+	const circle = document.querySelector(".circle")
+	cube.addEventListener('click', (e) => {
+		cube.classList.toggle("colors")
+		circle.classList.toggle("expand")
+		setInvertedText()
+	})
+})
