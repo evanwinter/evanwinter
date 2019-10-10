3f08229bb832bb7da2a9f7efacf470ceb941e5ea
Evan Winter <ewinter1996@gmail.com>
2019-10-09 19:33:47 -0500

    big cube, about to try something else

--- src/js/index.js
+++ src/js/index.js
@@ -6,22 +6,36 @@ import device from "current-device"
 
 import typography from "./typography"
 
-const App = {
+const randomColor = () => {
+	const colors = [
+		'red',
+		'white',
+		'blue',
+		'green'
+	]
+	const randomNumber = (min, max) => {
+		min = Math.ceil(min)
+		max = Math.floor(max)
+		return Math.floor(Math.random() * (max - min + 1)) + min
+	}
+	return colors[randomNumber(0, 3)]
+}
 
+const App = {
 	body: null,
 	cube: null,
 
 	init: function() {
 		this.body = document.querySelector("body")
 		this.cube = this.body.querySelector(".cube")
-
 		this.initCube()
 	},
 
 	initCube: function() {
 		this.cube.addEventListener("click", (e) => {
-			this.cube.classList.toggle("colors")
-			// this.invertTheme()
+			// this.body.style.background = randomColor()
+			this.invertTheme()
+			// this.invertCube()
 		})
 	},
 
@@ -29,6 +43,10 @@ const App = {
 		this.body.classList.toggle("inverted")
 	},
 
+	invertCube: function() {
+		// this.cube.classList.toggle("colors")
+		this.cube.classList.toggle("inverted")
+	},
 }
 
 document.addEventListener("DOMContentLoaded", () => {
